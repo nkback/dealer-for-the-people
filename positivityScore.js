2303bd17b2c51503aef0f4b30a0a12e79bd89c79
Nate Back <nkback@gmail.com>
2021-11-11 07:50:15 -0700

    Finish logic on the postivity score, and add several unit tests aroudn its functionality

--- positivityScore.js
+++ positivityScore.js
@@ -1,6 +1,34 @@
+const positiveWords = ["absolutely","accepted","acclaimed","accomplish","accomplishment","achievement","action","active","admire","adorable","adventure","affirmative","affluent","agree","agreeable","amazing","angelic","appealing","approve","aptitude","attractive","awesome","beaming","beautiful","believe","beneficial","bliss","bountiful","bounty","brave","bravo","brilliant","bubbly","calm","celebrated","certain","champ","champion","charming","cheery","choice","classic","classical","clean","commend","composed","congratulation","constant","cool","courageous","creative","cute","dazzling","delight","delightful","distinguished","divine","earnest","easy","ecstatic","effective","effervescent","efficient","effortless","electrifying","elegant","enchanting","encouraging","endorsed","energetic","energized","engaging","enthusiastic","essential","esteemed","ethical","excellent","exciting","exquisite","fabulous","fair","familiar","famous","fantastic","favorable","fetching","fine","fitting","flourishing","fortunate","free","fresh","friendly","fun","funny","generous","genius","genuine","giving","glamorous","glowing","good","gorgeous","graceful","great","green","grin","growing","handsome","happy","harmonious","healing","healthy","hearty","heavenly","honest","honorable","honored","hug","idea","ideal","imaginative","imagine","impressive","independent","innovate","innovative","instant","instantaneous","instinctive","intellectual","intelligent","intuitive","inventive","jovial","joy","jubilant","keen","kind","knowing","knowledgeable","laugh","learned","legendary","light","lively","lovely","lucid","lucky","luminous","marvelous","masterful","meaningful","merit","meritorious","miraculous","motivating","moving","natural","nice","novel","now","nurturing","nutritious","okay","one","one-hundred percent","open","optimistic","paradise","perfect","phenomenal","pleasant","pleasurable","plentiful","poised","polished","popular","positive","powerful","prepared","pretty","principled","productive","progress","prominent","protected","proud","quality","quick","quiet","ready","reassuring","refined","refreshing","rejoice","reliable","remarkable","resounding","respected","restored","reward","rewarding","right","robust","safe","satisfactory","secure","seemly","simple","skilled","skillful","smile","soulful","sparkling","special","spirited","spiritual","stirring","stunning","stupendous","success","successful","sunny","super","superb","supporting","surprising","terrific","thorough","thrilling","thriving","tops","tranquil","transformative","transforming","trusting","truthful","unreal","unwavering","up","upbeat","upright","upstanding","valued","vibrant","victorious","victory","vigorous","virtuous","vital","vivacious","wealthy","welcome","well","whole","wholesome","willing","wonderful","wondrous","worthy","wow","yes","yummy","zeal","zealous"];
+
 const getPositivityScore = (comment) => {
-    //Run analysis on comment and return a score
-    return 5;
+
+    if(typeof comment !== 'string' && !(comment instanceof String)) {
+        throw new TypeError("Comment needs to be a string")
+    }
+
+    let score = 2; //Start with a default score of 2 so multiplier won't have any issues if there aren't any positive word matches.
+
+    const singleReg = /(?<![!])(!)(?![!])/gm
+    score += (comment.match(singleReg) || []).length
+
+    const sequentialReg = /(!)\1+/gm
+    const matches = (comment.match(sequentialReg) || []) //We're gonna hold on to this until the end and just it as an exponential multiplier.
+
+    comment = comment.replace(/[^\w\s]|_/g, "").replace(/\s+/g, " "); //Get rid of punctuation
+
+    //Count each occurance of a positive word. 5pts each.
+    const wordArr = comment.split(" ");
+    const intersections = positiveWords.filter(value => wordArr.includes(value));
+    score += intersections.length * 5;
+
+    matches.forEach(item => {
+        score = Math.pow(score,item.length) //scores go up exponentially for sequential exclamation points.
+    })
+
+    return score;
 }
 
 exports.getPositivityScore = getPositivityScore;
+
+//(?<![!])(!)(?![!])
+//(!)\1+
--- positivityScore.test.js
+++ positivityScore.test.js
@@ -1,6 +1,58 @@
 const { test, expect } = require('@jest/globals')
 const {getPositivityScore} = require('./positivityScore')
 
-test('passing a comment with 5 exclamation points to getPostiveScore should return 5', () => {
-    expect(getPositivityScore('I loved this place!!!!!')).toBe(5);
+test('passing a comment with 5 exclamation points to getPostiveScore should return 32', () => {
+    expect(getPositivityScore('I loved this place!!!!!')).toBe(32);
+});
+
+test('passing a comment with absolutely, agreeable, and admire returns 17', () => {
+    expect(getPositivityScore('I absolutely admire their agreeable nature')).toBe(17);
+});
+
+test('passing a comment with no keywords or explanations should return 2', () => {
+    expect(getPositivityScore('This review is lame')).toBe(2);
+});
+
+test('passing a comment with a keyword, a single exlamation, and 3 sequential exclamations should return 512', () => {
+    expect(getPositivityScore('I got lucky! Will come again!!!')).toBe(512);
+});
+
+test('passing empty string should return 2', () => {
+    expect(getPositivityScore('')).toBe(2);
+});
+
+test('passing null should throw exception', () => {
+    try {
+        getPositivityScore(null)
+        expect(true).toBe(false);
+    } catch (e) {
+        expect(e.message).toBe("Comment needs to be a string");
+    }
+});
+
+test('passing number should throw exception', () => {
+    try {
+        getPositivityScore(5)
+        expect(true).toBe(false);
+    } catch (e) {
+        expect(e.message).toBe("Comment needs to be a string");
+    }
+});
+
+test('passing array should throw exception', () => {
+    try {
+        getPositivityScore([])
+        expect(true).toBe(false);
+    } catch (e) {
+        expect(e.message).toBe("Comment needs to be a string");
+    }
+});
+
+test('passing object should throw exception', () => {
+    try {
+        getPositivityScore({})
+        expect(true).toBe(false);
+    } catch (e) {
+        expect(e.message).toBe("Comment needs to be a string");
+    }
 });
